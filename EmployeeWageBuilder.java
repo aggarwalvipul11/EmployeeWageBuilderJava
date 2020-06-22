fd4dfd68b1f2cb21c1dc36f221c0452032504bc5
aggarwalvipul11 <vipul_dev@DESKTOP-QA3S37M.localdomain>
2020-06-22 07:32:04 +0530

    Completed UseCase7

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -5,7 +5,7 @@ public class EmployeeWageBuilder {
 	public static final int NUM_OF_WORKING_DAYS = 2;
 	public static final int MAX_HRS_IN_MONTH = 10;
 
-	public static void main(String[] args) {
+	public static int computeEmpWage() {
 		// Variables
 		int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0; 
 		// Computation
@@ -27,6 +27,11 @@ public class EmployeeWageBuilder {
 		}
 		int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
 		System.out.println("Total Emp Wage: " + totalEmpWage);
+		return totalEmpWage;
+	}
+	
+	public static void main(String[] args) {
+		computeEmpWage();
 	}
 }
 
